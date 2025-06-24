4952272f5f7859717d67824962bad4ce01ca4e7e
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-06-24 12:09:34 -0400

    Fix

--- nuevo_dev_gamabol/models/new_dev.py
+++ nuevo_dev_gamabol/models/new_dev.py
@@ -1,51 +1,26 @@
 
-# from odoo import fields, models
-
-# class ReportPosOrder(models.Model):
-#     _inherit = 'report.pos.order'
-
-#     barcode = fields.Char(string='Código de Barras', readonly=True)
-
-#     def _select(self):
-#         return f"""
-#             {super()._select()},
-#             pp.barcode AS barcode
-#         """
-
-#     def _from(self):
-#         return f"""
-#             {super()._from()}
-#             LEFT JOIN product_product pp ON pp.id = l.product_id
-#         """
-
-#     def _group_by(self):
-#         return f"""
-#             {super()._group_by()},
-#             pp.barcode
-#         """
-
-from odoo import models, fields
+from odoo import fields, models
 
 class ReportPosOrder(models.Model):
     _inherit = 'report.pos.order'
 
-    product_categ_id = fields.Many2one('product.category', string='Categoría del producto', readonly=True)
+    barcode = fields.Char(string='Código de Barras', readonly=True)
 
     def _select(self):
         return f"""
             {super()._select()},
-            pt.categ_id AS product_categ_id
+            pp.barcode AS barcode
         """
 
     def _from(self):
         return f"""
             {super()._from()}
             LEFT JOIN product_product pp ON pp.id = l.product_id
-            LEFT JOIN product_template pt ON pt.id = pp.product_tmpl_id
         """
 
     def _group_by(self):
         return f"""
             {super()._group_by()},
-            pt.categ_id
+            pp.barcode
         """
+
