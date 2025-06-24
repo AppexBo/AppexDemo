9d3c680e8006daa98fd436d06e828c052e2788f5
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-06-24 12:02:19 -0400

    Fix

--- nuevo_dev_gamabol/models/new_dev.py
+++ nuevo_dev_gamabol/models/new_dev.py
@@ -1,55 +1,51 @@
-# from odoo import models, fields, api
+
+# from odoo import fields, models
 
 # class ReportPosOrder(models.Model):
 #     _inherit = 'report.pos.order'
 
-#     has_barcode = fields.Boolean(
-#         string="¿Tiene código de barras?",
-#         compute='_compute_has_barcode',
-#         store=False,
-#         search='_search_has_barcode'
-#     )
+#     barcode = fields.Char(string='Código de Barras', readonly=True)
 
-#     def _compute_has_barcode(self):
-#         for rec in self:
-#             rec.has_barcode = False  # No se muestra en pantalla, solo sirve para el filtro
+#     def _select(self):
+#         return f"""
+#             {super()._select()},
+#             pp.barcode AS barcode
+#         """
 
-#     @api.model
-#     def _search_has_barcode(self, operator, value):
-#         # value = True => productos CON código de barras
-#         # value = False => productos SIN código de barras
-#         product_ids = self.env['product.product'].search([
-#             ('barcode', '!=', False) if value else ('barcode', '=', False)
-#         ]).ids
+#     def _from(self):
+#         return f"""
+#             {super()._from()}
+#             LEFT JOIN product_product pp ON pp.id = l.product_id
+#         """
 
-#         order_line_ids = self.env['pos.order.line'].search([
-#             ('product_id', 'in', product_ids)
-#         ]).mapped('order_id.id')
+#     def _group_by(self):
+#         return f"""
+#             {super()._group_by()},
+#             pp.barcode
+#         """
 
-#         return [('order_id', 'in', order_line_ids)]
-from odoo import fields, models
+from odoo import models, fields
 
 class ReportPosOrder(models.Model):
     _inherit = 'report.pos.order'
 
-    barcode = fields.Char(string='Código de Barras', readonly=True)
+    product_categ_id = fields.Many2one('product.category', string='Categoría del producto', readonly=True)
 
     def _select(self):
         return f"""
             {super()._select()},
-            pp.barcode AS barcode
+            pt.categ_id AS product_categ_id
         """
 
     def _from(self):
         return f"""
             {super()._from()}
             LEFT JOIN product_product pp ON pp.id = l.product_id
+            LEFT JOIN product_template pt ON pt.id = pp.product_tmpl_id
         """
 
     def _group_by(self):
         return f"""
             {super()._group_by()},
-            pp.barcode
+            pt.categ_id
         """
-
-
