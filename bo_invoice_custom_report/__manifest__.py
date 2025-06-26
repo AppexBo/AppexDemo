0bcf762cef57fe4d55452b0451f60d15faacf8c1
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-06-26 18:02:35 -0400

    Reporte fix

--- bo_invoice_custom_report/__manifest__.py
+++ bo_invoice_custom_report/__manifest__.py
@@ -3,14 +3,9 @@
     'version': '17.0.1.0.0',
     'summary': 'Personaliza el reporte fiscal boliviano de l10n_bo',
     'author': 'Tu Nombre',
-    'depends': ['account','l10n_bo_bolivian_invoice'],
+    'depends': ['base','account','l10n_bo_bolivian_invoice'],
     'data': [
-        'views/paper_format.xml',
         'views/report_roll_inherit.xml',
-        
-        'views/boton.xml',
-        'views/cuerpo.xml',
-
     ],
     'installable': True,
     'auto_install': False,
