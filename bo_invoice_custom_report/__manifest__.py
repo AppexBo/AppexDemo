3b2fb83dd8edb631eed1648e8ebde3a3cf4eeb91
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-06-26 19:22:11 -0400

    Reporte fix

--- bo_invoice_custom_report/__manifest__.py
+++ bo_invoice_custom_report/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Personalización de Reporte Boliviano 18',
     'version': '17.0.1.0.0',
     'summary': 'Personaliza el reporte fiscal boliviano de l10n_bo',
-    'author': 'Tu Nombre',
+    'author': 'APPEX BOLIVIA SRL.',
     'depends': ['base','account','l10n_bo_bolivian_invoice'],
     'data': [
         'views/report_roll_inherit.xml',
