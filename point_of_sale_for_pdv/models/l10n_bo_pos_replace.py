e1ee62c6dd1c0c425b83e9c90e0f122ac46fd06a
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-06-28 15:44:37 -0400

    Fix

--- point_of_sale_for_pdv/models/l10n_bo_pos_replace.py
+++ point_of_sale_for_pdv/models/l10n_bo_pos_replace.py
@@ -8,59 +8,61 @@ from pytz import timezone
 import pytz
 _logger = logging.getLogger(__name__)
 
+
 class L10nBoPos(models.Model):
-    _inherit ="l10n.bo.pos"
-    _description="Nuevos campos en el punto de venta"
+    _inherit = "l10n.bo.pos"
+    _description = "Nuevos campos en el punto de venta"
 
     company_id = fields.Many2one(
-        string='Compañia', 
-        comodel_name='res.company', 
-        required=True, 
+        string='Compañia',
+        comodel_name='res.company',
+        required=True,
         default=lambda self: self.env.company,
-        readonly=True 
-        
+        readonly=True
+
     )
     state_id = fields.Many2one(
         string='Departamento',
         comodel_name='res.country.state',
-        domain=lambda self: [('country_id', '=', self.env.company.country_id.id)]
+        domain=lambda self: [
+            ('country_id', '=', self.env.company.country_id.id)]
     )
-    
+
     province_id = fields.Many2one(
         string='Provincia',
         comodel_name='res.city',
         copy=False
     )
-    
+
     municipality_id = fields.Many2one(
         string='Municipio',
         comodel_name='res.municipality',
         copy=False,
-    )   
+    )
+
+    @api.onchange('state_id')
+    def _onchange_state_id(self):
+        if self.state_id and self.company_id:
+            self.company_id.state_id = self.state_id
+
+    @api.onchange('province_id')
+    def _onchange_province_id(self):
+        if self.province_id and self.company_id:
+            self.company_id.province_id = self.province_id
+
+    @api.onchange('municipality_id')
+    def _onchange_municipality_id(self):
+        if self.municipality_id and self.company_id:
+            self.company_id.municipality_id = self.municipality_id
+
     @api.onchange('state_id')
     def _onchange_state_id(self):
         if self._origin and self.state_id and self.state_id != self._origin.state_id:
             self.province_id = False
             self.municipality_id = False
-    
+
     def getMunicipalityName(self):
         if self.municipality_id:
             return self.municipality_id.name
-        raise UserError('El punto de venta seleccionado no tiene municipio asignado.')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        raise UserError(
+            'El punto de venta seleccionado no tiene municipio asignado.')
