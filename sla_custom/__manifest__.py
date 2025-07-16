9460dc4bbe2282d50d8c554f53f80e8f7ec91414
Yamil Torrico Vasquez <Yamilvasqueztorrico@gmail.com>
2025-07-16 11:26:19 -0400

    Modulo SLA prueba 1

--- sla_custom/__manifest__.py
+++ sla_custom/__manifest__.py
@@ -2,15 +2,13 @@
     'name': 'SLA Custom, APPEX BOLIVIA',
     'version': '1.0',
     'category': '',
-    'description':'By for Appex',
+    'description': 'By for Appex',
     'summary': 'SLA',
     'depends': ['helpdesk'],
-    'assets': {
-        'point_of_sale._assets_pos': [
-            'recibo_punto_venta/static/src/xml/custom_receipt.xml',
-        ],
-    },
-  
+    'data': [
+        'views/vista.xml',
+    ],
+
     'installable': True,
     'application': False,
 }
